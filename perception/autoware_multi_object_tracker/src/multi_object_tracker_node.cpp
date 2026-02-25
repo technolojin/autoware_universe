db2c29a90938cff5bb43dda7546aa04783056db3
Taekjin LEE <taekjin.lee@tier4.jp>
2026-02-25 12:07:18 +0900

    refactor(multi_object_tracker): improve parameter declaration structure and readability
    - Enhanced the organization of parameter declarations within the MultiObjectTracker constructor for better clarity.
    - Streamlined the input channel configuration process by consolidating related logic, improving maintainability.
    - Updated comments to reflect changes and improve overall code readability.
    
    This commit enhances the clarity and maintainability of the multi-object tracking system within the Autoware framework.
    
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- perception/autoware_multi_object_tracker/src/multi_object_tracker_node.cpp
+++ perception/autoware_multi_object_tracker/src/multi_object_tracker_node.cpp
@@ -45,7 +45,7 @@ using LabelType = autoware_perception_msgs::msg::ObjectClassification::_label_ty
 MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
 : rclcpp::Node("multi_object_tracker", node_options)
 {
-  // Get parameters
+  ////// Get parameters
   params_.publish_rate = declare_parameter<double>("publish_rate");  // [hz]
   params_.world_frame_id = declare_parameter<std::string>("world_frame_id");
   params_.ego_frame_id = declare_parameter<std::string>("ego_frame_id");
@@ -55,129 +55,126 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
     declare_parameter<bool>("publish_processing_time_detail");
   params_.publish_merged_objects = declare_parameter<bool>("publish_merged_objects");
 
-  // ROS interface - Input channels
   // define input channel parameters
-  std::vector<std::string> input_channels;
-  std::vector<std::string> input_channel_topics;
-  input_channels.resize(types::max_channel_size);
-  input_channel_topics.resize(types::max_channel_size);
-  for (size_t i = 0; i < types::max_channel_size; i++) {
-    // the index number is zero filled two digits format
-    const int index = static_cast<int>(i + 1);
-    const std::string channel_id =
-      std::string("detection") + (index < 10 ? "0" : "") + std::to_string(index);
-    input_channels.at(i) = declare_parameter<std::string>("input/" + channel_id + "/channel");
-    input_channel_topics.at(i) = declare_parameter<std::string>("input/" + channel_id + "/objects");
-  }
-
-  // parse input channels
-  uint channel_index = 0;
-  for (size_t i = 0; i < types::max_channel_size; i++) {
-    const std::string & input_channel = input_channels.at(i);
-    const std::string & input_channel_topic = input_channel_topics.at(i);
-    if (input_channel.empty() || input_channel == "none") {
-      continue;
+  {
+    std::vector<std::string> input_channels;
+    std::vector<std::string> input_channel_topics;
+    input_channels.resize(types::max_channel_size);
+    input_channel_topics.resize(types::max_channel_size);
+    for (size_t i = 0; i < types::max_channel_size; i++) {
+      // the index number is zero filled two digits format
+      const int index = static_cast<int>(i + 1);
+      const std::string channel_id =
+        std::string("detection") + (index < 10 ? "0" : "") + std::to_string(index);
+      input_channels.at(i) = declare_parameter<std::string>("input/" + channel_id + "/channel");
+      input_channel_topics.at(i) =
+        declare_parameter<std::string>("input/" + channel_id + "/objects");
     }
 
-    types::InputChannel input_channel_config;
-    input_channel_config.index = channel_index;
-    channel_index++;
+    // parse input channels
+    uint channel_index = 0;
+    for (size_t i = 0; i < types::max_channel_size; i++) {
+      const std::string & input_channel = input_channels.at(i);
+      const std::string & input_channel_topic = input_channel_topics.at(i);
+      if (input_channel.empty() || input_channel == "none") {
+        continue;
+      }
 
-    // topic name
-    input_channel_config.input_topic = input_channel_topic;
-    // required parameter, but can set a default value
-    input_channel_config.is_spawn_enabled = declare_parameter<bool>(
-      "input_channels." + input_channel + ".flags.can_spawn_new_tracker", true);
+      types::InputChannel input_channel_config;
+      input_channel_config.index = channel_index;
+      channel_index++;
 
-    // trust object existence probability
-    input_channel_config.trust_existence_probability = declare_parameter<bool>(
-      "input_channels." + input_channel + ".flags.can_trust_existence_probability", false);
+      // topic name
+      input_channel_config.input_topic = input_channel_topic;
+      // required parameter, but can set a default value
+      input_channel_config.is_spawn_enabled = declare_parameter<bool>(
+        "input_channels." + input_channel + ".flags.can_spawn_new_tracker", true);
 
-    // trust object extension, size beyond the visible area
-    input_channel_config.trust_extension = declare_parameter<bool>(
-      "input_channels." + input_channel + ".flags.can_trust_extension", true);
+      // trust object existence probability
+      input_channel_config.trust_existence_probability = declare_parameter<bool>(
+        "input_channels." + input_channel + ".flags.can_trust_existence_probability", false);
 
-    // trust object classification
-    input_channel_config.trust_classification = declare_parameter<bool>(
-      "input_channels." + input_channel + ".flags.can_trust_classification", true);
+      // trust object extension, size beyond the visible area
+      input_channel_config.trust_extension = declare_parameter<bool>(
+        "input_channels." + input_channel + ".flags.can_trust_extension", true);
 
-    // trust object orientation(yaw)
-    input_channel_config.trust_orientation = declare_parameter<bool>(
-      "input_channels." + input_channel + ".flags.can_trust_orientation", true);
+      // trust object classification
+      input_channel_config.trust_classification = declare_parameter<bool>(
+        "input_channels." + input_channel + ".flags.can_trust_classification", true);
 
-    // optional parameters
-    const std::string default_name = input_channel;
-    const std::string name_long = declare_parameter<std::string>(
-      "input_channels." + input_channel + ".optional.name", default_name);
-    input_channel_config.long_name = name_long;
+      // trust object orientation(yaw)
+      input_channel_config.trust_orientation = declare_parameter<bool>(
+        "input_channels." + input_channel + ".flags.can_trust_orientation", true);
 
-    const std::string default_name_short = input_channel.substr(0, 3);
-    const std::string name_short = declare_parameter<std::string>(
-      "input_channels." + input_channel + ".optional.short_name", default_name_short);
-    input_channel_config.short_name = name_short;
+      // optional parameters
+      const std::string default_name = input_channel;
+      const std::string name_long = declare_parameter<std::string>(
+        "input_channels." + input_channel + ".optional.name", default_name);
+      input_channel_config.long_name = name_long;
 
-    params_.input_channels_config.push_back(input_channel_config);
+      const std::string default_name_short = input_channel.substr(0, 3);
+      const std::string name_short = declare_parameter<std::string>(
+        "input_channels." + input_channel + ".optional.short_name", default_name_short);
+      input_channel_config.short_name = name_short;
+
+      params_.input_channels_config.push_back(input_channel_config);
+    }
   }
 
-  // Parameters for processor
-  {
-    params_.tracker_type_map["car_tracker"] = declare_parameter<std::string>("car_tracker");
-    params_.tracker_type_map["truck_tracker"] = declare_parameter<std::string>("truck_tracker");
-    params_.tracker_type_map["bus_tracker"] = declare_parameter<std::string>("bus_tracker");
-    params_.tracker_type_map["trailer_tracker"] = declare_parameter<std::string>("trailer_tracker");
-    params_.tracker_type_map["pedestrian_tracker"] =
-      declare_parameter<std::string>("pedestrian_tracker");
-    params_.tracker_type_map["bicycle_tracker"] = declare_parameter<std::string>("bicycle_tracker");
-    params_.tracker_type_map["motorcycle_tracker"] =
-      declare_parameter<std::string>("motorcycle_tracker");
+  // tracker type map
+  params_.tracker_type_map["car_tracker"] = declare_parameter<std::string>("car_tracker");
+  params_.tracker_type_map["truck_tracker"] = declare_parameter<std::string>("truck_tracker");
+  params_.tracker_type_map["bus_tracker"] = declare_parameter<std::string>("bus_tracker");
+  params_.tracker_type_map["trailer_tracker"] = declare_parameter<std::string>("trailer_tracker");
+  params_.tracker_type_map["pedestrian_tracker"] =
+    declare_parameter<std::string>("pedestrian_tracker");
+  params_.tracker_type_map["bicycle_tracker"] = declare_parameter<std::string>("bicycle_tracker");
+  params_.tracker_type_map["motorcycle_tracker"] =
+    declare_parameter<std::string>("motorcycle_tracker");
 
-    // Declare parameters
-    params_.processor_config.tracker_lifetime = declare_parameter<double>("tracker_lifetime");
-    params_.processor_config.min_known_object_removal_iou =
-      declare_parameter<double>("min_known_object_removal_iou");
-    params_.processor_config.min_unknown_object_removal_iou =
-      declare_parameter<double>("min_unknown_object_removal_iou");
+  params_.processor_config.tracker_lifetime = declare_parameter<double>("tracker_lifetime");
+  params_.processor_config.min_known_object_removal_iou =
+    declare_parameter<double>("min_known_object_removal_iou");
+  params_.processor_config.min_unknown_object_removal_iou =
+    declare_parameter<double>("min_unknown_object_removal_iou");
 
-    // Declare parameters for generalized IoU threshold
-    params_.pruning_giou_thresholds =
-      declare_parameter<std::vector<double>>("pruning_generalized_iou_thresholds");
+  // pruning parameters
+  params_.pruning_giou_thresholds =
+    declare_parameter<std::vector<double>>("pruning_generalized_iou_thresholds");
+  params_.processor_config.pruning_static_object_speed =
+    declare_parameter<double>("pruning_static_object_speed");
+  params_.processor_config.pruning_moving_object_speed =
+    declare_parameter<double>("pruning_moving_object_speed");
+  params_.processor_config.pruning_static_iou_threshold =
+    declare_parameter<double>("pruning_static_iou_threshold");
 
-    params_.processor_config.pruning_static_object_speed =
-      declare_parameter<double>("pruning_static_object_speed");
-    params_.processor_config.pruning_moving_object_speed =
-      declare_parameter<double>("pruning_moving_object_speed");
-    params_.processor_config.pruning_static_iou_threshold =
-      declare_parameter<double>("pruning_static_iou_threshold");
-
-    // Declare parameters for overlap distance threshold
-    params_.pruning_distance_thresholds =
-      declare_parameter<std::vector<double>>("pruning_distance_thresholds");
-
-    params_.processor_config.enable_unknown_object_velocity_estimation =
-      declare_parameter<bool>("enable_unknown_object_velocity_estimation");
-    params_.processor_config.enable_unknown_object_motion_output =
-      declare_parameter<bool>("enable_unknown_object_motion_output");
-  }
+  // overlap distance threshold
+  params_.pruning_distance_thresholds =
+    declare_parameter<std::vector<double>>("pruning_distance_thresholds");
+  params_.processor_config.enable_unknown_object_velocity_estimation =
+    declare_parameter<bool>("enable_unknown_object_velocity_estimation");
+  params_.processor_config.enable_unknown_object_motion_output =
+    declare_parameter<bool>("enable_unknown_object_motion_output");
 
   // Parameters for associator
-  {
-    params_.can_assign_matrix = this->declare_parameter<std::vector<int64_t>>("can_assign_matrix");
-    params_.max_dist_matrix = this->declare_parameter<std::vector<double>>("max_dist_matrix");
-    params_.max_area_matrix = this->declare_parameter<std::vector<double>>("max_area_matrix");
-    params_.min_area_matrix = this->declare_parameter<std::vector<double>>("min_area_matrix");
-    params_.min_iou_matrix = this->declare_parameter<std::vector<double>>("min_iou_matrix");
+  params_.can_assign_matrix = this->declare_parameter<std::vector<int64_t>>("can_assign_matrix");
+  params_.max_dist_matrix = this->declare_parameter<std::vector<double>>("max_dist_matrix");
+  params_.max_area_matrix = this->declare_parameter<std::vector<double>>("max_area_matrix");
+  params_.min_area_matrix = this->declare_parameter<std::vector<double>>("min_area_matrix");
+  params_.min_iou_matrix = this->declare_parameter<std::vector<double>>("min_iou_matrix");
 
-    // Set the unknown-unknown association GIoU threshold
-    params_.associator_config.unknown_association_giou_threshold =
-      declare_parameter<double>("unknown_association_giou_threshold");
-  }
+  // Set the unknown-unknown association GIoU threshold
+  params_.associator_config.unknown_association_giou_threshold =
+    declare_parameter<double>("unknown_association_giou_threshold");
 
+  // process parameters
   core::process_parameters(params_);
 
-  // Initialize state
+  ////// Initialize state
   state_.init(params_, *this, std::bind(&MultiObjectTracker::onTrigger, this));
 
-  // Create subscriptions
+  ////// Create subscriptions and publishers
+  // subscriptions
   const size_t input_size = params_.input_channels_config.size();
   sub_objects_array_.resize(input_size);
   for (size_t i = 0; i < input_size; i++) {
@@ -194,7 +191,7 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
       channel.input_topic, rclcpp::QoS{1}, func);
   }
 
-  // ROS interface - Publisher
+  // publishers
   tracked_objects_pub_ = create_publisher<autoware_perception_msgs::msg::TrackedObjects>(
     "output/objects", rclcpp::QoS{1});
   if (params_.publish_merged_objects) {
@@ -215,9 +212,8 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
       }
     }
   }
-  // Create ROS time based timer.
-  // If the delay compensation is enabled, the timer is used to publish the output at the correct
-  // time.
+
+  ////// callback timer
   if (params_.enable_delay_compensation) {
     // const double publisher_period = 1.0 / params_.publish_rate;    // [s]
     constexpr double timer_multiplier = 10.0;  // 10 times frequent for publish timing check
@@ -226,7 +222,7 @@ MultiObjectTracker::MultiObjectTracker(const rclcpp::NodeOptions & node_options)
       this, get_clock(), timer_period, std::bind(&MultiObjectTracker::onTimer, this));
   }
 
-  // Debugger initialization
+  ////// Debugger
   debugger_ = std::make_unique<TrackerDebugger>(
     get_logger(), get_clock(), params_.world_frame_id, params_.input_channels_config);
   debugger_->init(*this);
